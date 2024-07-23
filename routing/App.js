002ea807b125e821a5c47c604e3cfba91b0a7a23
varshinik444 <152681241+varshinik444@users.noreply.github.com>
2024-07-23 22:46:57 +0530

    Add files via upload

---
+++ routing/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
